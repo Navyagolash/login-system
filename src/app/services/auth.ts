493aa7c473d7802b3e5c41827e53b830864127dd
Navya Golash <navya.golash@imperialplatforms.com>
2025-10-05 14:49:49 +0530

    redeploy vercel

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class Auth {
-  private baseUrl = 'http://localhost:5000/api/auth';
+  private baseUrl = 'https://auth-backend-nnnr.onrender.com/api/auth';
 
   constructor(private http: HttpClient) { }
 
